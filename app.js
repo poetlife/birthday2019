288f0e9035928b31c076a3554e81554c851ed8d6
pengjidong <976569380@qq.com>
2018-06-17 01:14:21 +0800

    koa -bodyparser use

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const Koa = require("koa2");
 const router = require("koa-router")();
+const bodyparser = require("koa-bodyparser");
 
 const app = new Koa();
 
@@ -17,10 +18,29 @@ router.get("/hello/:name", async (ctx, next)=>{
 })
 
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password" value="12345"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+});
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
 });
 
 // add middlewares
+app.use(bodyparser());
 app.use(router.routes());
 
 app.listen(3000);
