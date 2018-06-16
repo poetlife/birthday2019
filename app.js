dcea6abebbca497448984cf19fb0fbdbec9a8591
pengjidong <976569380@qq.com>
2018-06-17 00:44:11 +0800

    change dependencies

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const Koa = require("koa2");
+const router = require("koa-router")();
 
 const app = new Koa();
 
-app.use(async(ctx, next)=>{
+app.use(async (ctx, next)=>{
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
-    ctx.response.type = "text/html";
-    ctx.response.body = "<h1>Hello Uozoyo day 1</h1>";
 })
+
+// add router
+router.get("/hello/:name", async (ctx, next)=>{
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}</h1>`;
+})
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add middlewares
+app.use(router.routes());
 
 app.listen(3000);
